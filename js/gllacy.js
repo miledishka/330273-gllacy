8b34016a397c8d18f163920caed999b1f95380d7
Julija Rodionova <julija.rodionowa@yandex.ru>
2017-12-04 14:00:16 +0300

    поправлена иконка поиска, поправлена наведение кнопки вход, изменены состояния чекбоксов и радиобатонов, поправлен код в js и html, поправлены ссылки активности при наведении в разделе товара, поправлен блок модального окна: размеры подведены под пиксель перфект, крестик заменен на svg картинку, появление модального окна центрируется согласно тз.

--- js/gllacy.js
+++ js/gllacy.js
@@ -1,14 +1,14 @@
 // слайдер
-var sliderItems = document.querySelectorAll('.slider__item');
-var sliderControls = document.querySelectorAll('.slider__btn');
-var body = document.querySelector('body');
+var sliderItems = document.querySelectorAll(".slider__item");
+var sliderControls = document.querySelectorAll(".slider__btn");
+var body = document.querySelector("body");
 var setNewSlide = function(count) {
-	document.querySelector('.slider__item--active').classList.remove('slider__item--active');
-	sliderItems[count].classList.add('slider__item--active');
+	document.querySelector(".slider__item--active").classList.remove("slider__item--active");
+	sliderItems[count].classList.add("slider__item--active");
 }
 
 var setNewBackground = function(index) {
-  classes = Array('home--first', 'home--second', 'home--third');
+  classes = Array("home--first", "home--second", "home--third");
   classes.map(function(item) {
     return body.classList.remove(item);
   });
@@ -16,13 +16,13 @@ var setNewBackground = function(index) {
 }
 
 for(var i = 0; i < sliderControls.length; i++) {
-	sliderControls[i].addEventListener('click', function(e) {
+	sliderControls[i].addEventListener("click", function(e) {
 		e.preventDefault();
 
 		var index = [].indexOf.call(sliderControls, this);
 
-		document.querySelector('.slider__btn--active').classList.remove('slider__btn--active');
-		this.classList.add('slider__btn--active');
+		document.querySelector(".slider__btn--active").classList.remove("slider__btn--active");
+		this.classList.add("slider__btn--active");
 
 		setNewSlide(index);
 		setNewBackground(index);
@@ -30,24 +30,24 @@ for(var i = 0; i < sliderControls.length; i++) {
 }
 
 // карта
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener("DOMContentLoaded", function(){
   ymaps.ready(function () {
-      var myMap = new ymaps.Map('yandex-map', {
-              center: [59.939319, 30.328747],
-              zoom: 16
-          }, {
-              searchControlProvider: 'yandex#search'
-          }),
+    var myMap = new ymaps.Map("yandex-map", {
+      center: [59.939319, 30.328747],
+      zoom: 16
+      }, {
+      searchControlProvider: "yandex#search"
+      }),
 
-          myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
-          }, {
-              iconLayout: 'default#image',
-              iconImageHref: './img/pin.svg',
-              iconImageSize: [80, 140],
-              iconImageOffset: [-40, -140]
-          });
+  myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
+    }, {
+      iconLayout: "default#image",
+      iconImageHref: "./img/pin.svg",
+      iconImageSize: [80, 140],
+      iconImageOffset: [-40, -140]
+    });
 
-      myMap.geoObjects.add(myPlacemark);
+  myMap.geoObjects.add(myPlacemark);
   });
 });
 
@@ -58,22 +58,21 @@ function toggle(id, toggled_class) {
   if (element.classList) {
     element.classList.toggle(toggled_class);
  } else {
-      // For IE9
-      var classes = element.className.split(" ");
-      var i = classes.indexOf(toggled_class);
-   if (i >= 0)
-         classes.splice(i, 1);
-      else
-         classes.push(toggled_class);
-          element.className = classes.join(" ");
+    var classes = element.className.split(" ");
+    var i = classes.indexOf(toggled_class);
+  if (i >= 0)
+    classes.splice(i, 1);
+    else
+    classes.push(toggled_class);
+    element.className = classes.join(" ");
   }
 }
 
 function toggleModal(id) {
-  toggle(id, 'active');
-  toggle('modal-overlay', 'active');
+  toggle(id, "active");
+  toggle("modal-overlay", "active");
   modal = document.getElementById(id);
-  overlay = document.getElementById('modal-overlay');
+  overlay = document.getElementById("modal-overlay");
   removeError(modal, overlay);
 }
 
@@ -92,7 +91,7 @@ var commentFeedback = form.querySelector("[name=comment-feedback]");
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27 && modal.classList.contains("active")) {
-    toggleModal('feedback-modal');
+    toggleModal("feedback-modal");
   }
 });
 
