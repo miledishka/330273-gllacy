e44a77fba61e0d5834a36ca703b4757f0c790270
Julija Rodionova <julija.rodionowa@yandex.ru>
2017-12-05 15:10:14 +0300

    поправлены отступы в html (главная, каталог) и в js (карта)

--- js/gllacy.js
+++ js/gllacy.js
@@ -35,21 +35,21 @@ document.addEventListener("DOMContentLoaded", function(){
     var myMap = new ymaps.Map("yandex-map", {
       center: [59.939319, 30.328747],
       zoom: 16
-      }, {
-      searchControlProvider: "yandex#search"
-      }),
-
-  myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
     }, {
+      searchControlProvider: "yandex#search"
+    }),
+
+    myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {}, {
       iconLayout: "default#image",
       iconImageHref: "./img/pin.svg",
       iconImageSize: [80, 140],
       iconImageOffset: [-40, -140]
     });
 
-  myMap.geoObjects.add(myPlacemark);
+    myMap.geoObjects.add(myPlacemark);
   });
 });
+
 
 // модалка
 function toggle(id, toggled_class) {
